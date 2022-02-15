f07cdc8d2e11b49e90e5f987de4fa392bec66a1c
Hedieh Heydari <heydari.hedieh2680@gmail.com>
2022-02-15 23:35:53 +0330

    bootstrap cdn added

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const input = document.querySelector("input");
 const message = document.querySelector(".msg");
 const list = document.querySelector(".cities");
 
-
 const apiKey = "825a05620926077b02fae8b681a21e2c";
 
 form.addEventListener("submit", e => {
@@ -16,7 +15,6 @@ form.addEventListener("submit", e => {
             console.log(data);
             const {main, name, sys, weather, wind, } = data;
             const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
- 
             const li = document.createElement("li");
             li.classList.add("city");
             const markup = `
@@ -28,12 +26,11 @@ form.addEventListener("submit", e => {
             <figure>
             <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
             <figcaption>${weather[0]["description"]}</figcaption>
-          </figure>
+            </figure>
             <div class='city-name'>Wind speed: ${Math.round(wind.speed)} m/s</div>
             <div class='city-name'>Humidity: ${Math.round(main.humidity)}%</div>
             <div class='city-name'>Min temp: ${Math.round(main.temp_min)}</div>
             <div class='city-name'>Max temp: ${Math.round(main.temp_max)}</div>
-
             `;
             li.innerHTML=markup;
             list.appendChild(li);
